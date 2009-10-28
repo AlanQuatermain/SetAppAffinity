25c5723a42338a672b88d0ec3b793c09798abf9b
Jim Dovey <jimdovey@mac.com>
2009-10-28 18:20:00 -0400

    Initial import; compiles but untested.

---
+++ SetAppAffinity.m
@@ -0,0 +1,229 @@
+/*
+ * SetAppAffinity.m
+ * SetAppAffinity
+ * 
+ * Created by Jim Dovey on 28/10/2009.
+ * 
+ * Copyright (c) 2009 Jim Dovey
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the name of the project's author nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#import <Foundation/Foundation.h>
+#import <AppKit/NSWorkspace.h>
+#import <AppKit/NSImage.h>
+#import <CoreServices/CoreServices.h>
+
+#import <getopt.h>
+#import <sysexits.h>
+#import <sys/syslimits.h>
+
+static const char *		_shortCommandLineArgs = "ha:b:";
+static struct option	_longCommandLineArgs[] = {
+	{ "help", no_argument, NULL, 'h' },
+	{ "app-path", required_argument, NULL, 'a' },
+	{ "bundle-id", required_argument, NULL, 'b' },
+	{ NULL, 0, NULL, 0 }
+};
+
+static void usage(FILE *, int) __dead2;
+
+static void usage( FILE * fp, int exitCode )
+{
+	const char * procName = [[[NSProcessInfo processInfo] processName] UTF8String];
+	fprintf( fp,
+			 "Usage:\n"
+			 "  %s [-h | --help]					Display this list of options.\n",
+			 "  %s [-a | --app-path] <file-path>	Set file at <file-path> to open using\n"
+			 "                                      application bundle at <app-path>.\n"
+			 "  %s [-b | --bundle-id] <file-path>	Set file at <file-path> to open using\n"
+			 "                                      application with the given bundle id.\n",
+			 procName, procName, procName );
+	fflush( fp );
+	exit( exitCode );
+}
+
+static void RemoveUsroResource( NSURL * fileURL )
+{
+	FSRef fsRef;
+	if ( CFURLGetFSRef((CFURLRef)fileURL, &fsRef) == FALSE )
+	{
+		fprintf( stderr, "Failed to access input file.\n" );
+		exit( EX_OSERR );
+	}
+	
+	ResFileRefNum refnum = FSOpenResFile( &fsRef, fsRdWrPerm );
+	
+	Handle oldUsro = Get1Resource( 'usro', 0 );
+	if ( oldUsro != NULL )
+		RemoveResource( oldUsro );	// this invalidates the handle
+	
+	CloseResFile( refnum );
+}
+
+static BOOL InstallUsroResource( NSURL * fileURL, NSURL * appURL )
+{
+	FSRef fileRef;
+	if ( CFURLGetFSRef((CFURLRef)fileURL, &fileRef) == FALSE )
+	{
+		fprintf( stderr, "Failed to access input file.\n" );
+		exit( EX_OSERR );
+	}
+	
+	Handle newUsro = NewHandleClear( PATH_MAX + sizeof(UInt32) );
+	char * bytes = (char *) *newUsro;	// a Handle is a ptr to a ptr
+	
+	const char * path = [[appURL path] UTF8String];
+	UInt32 len = (UInt32) MIN(strlen(path), PATH_MAX);
+	
+	// copy the data in: 32-bit length followed by path (similar to a HFS string)
+	memcpy( bytes, &len, sizeof(UInt32) );
+	memcpy( (bytes+sizeof(UInt32)), path, len );
+	
+	ResFileRefNum refnum = FSOpenResFile( &fileRef, fsRdWrPerm );
+	
+	// add the resource to the file
+	AddResource( newUsro, 'usro', 0, "\p" );	// last param is an empty Pascal string
+	DisposeHandle( newUsro );					// release the handle memory
+	
+	BOOL result = (ResError() == noErr);
+	
+	CloseResFile( refnum );
+	return ( result );
+}
+
+NSImage * GetAppFileIcon( NSURL * appURL, NSURL * fileURL, NSString * uti )
+{
+	NSBundle * appBundle = [NSBundle bundleWithURL: appURL];
+	NSArray * docTypes = [[appBundle infoDictionary] objectForKey: @"CFBundleDocumentTypes"];
+	NSString * extension = [fileURL pathExtension];
+	NSString * iconName = nil;
+	
+	for ( NSDictionary * docType in docTypes )
+	{
+		if ( ([[docType objectForKey: @"LSItemContentTypes"] containsObject: uti]) ||
+			 ([[docType objectForKey: @"CFBundleTypeExtensions"] containsObject: extension]) )
+		{
+			iconName = [docType objectForKey: @"CFBundleTypeIconFile"];
+			break;
+		}
+	}
+	
+	if ( iconName == nil )
+		return ( nil );
+	
+	return ( [[NSImage alloc] initWithContentsOfURL: [appBundle URLForImageResource: iconName]] );
+}
+
+int main (int argc, const char * argv[])
+{
+	NSURL * appURL = nil;
+    
+	int ch = 0;
+	while ( (ch = getopt_long(argc, (char **)argv, _shortCommandLineArgs, _longCommandLineArgs, NULL)) != -1 )
+	{
+		switch ( ch )
+		{
+			default:
+				usage( stderr, EX_USAGE );	// dead call
+				break;
+			
+			case 'h':
+				usage( stdout, EX_OK );	// dead call, exits app
+				break;
+				
+			case 'a':
+			{
+				NSString * path = [NSString stringWithUTF8String: optarg];
+				if ( [[NSFileManager defaultManager] fileExistsAtPath: [appURL path]] == NO )
+				{
+					fprintf( stderr, "Unable to locate application at path '%s'\n", optarg );
+					fflush( stderr );
+					exit( EX_DATAERR );
+				}
+				
+				appURL = [NSURL fileURLWithPath: path];
+				break;
+			}
+				
+			case 'b':
+			{
+				appURL = [[NSWorkspace sharedWorkspace] URLForApplicationWithBundleIdentifier: [NSString stringWithUTF8String: optarg]];
+				if ( appURL == nil )
+				{
+					fprintf( stderr, "Unable to locate application with identifier '%s'\n", optarg );
+					fflush( stderr );
+					exit( EX_DATAERR );
+				}
+				break;
+			}
+		}
+	}
+	
+	if ( optind >= argc )
+		usage( stderr, EX_USAGE );
+	
+	NSURL * fileURL = [NSURL fileURLWithPath: [NSString stringWithUTF8String: argv[optind]]];
+	
+	if ( [[NSFileManager defaultManager] fileExistsAtPath: [fileURL path]] == NO )
+	{
+		fprintf( stderr, "File not found: %s", argv[optind] );
+		fflush( stderr );
+		exit( EX_DATAERR );
+	}
+	
+	// got all the URLs we need now
+	
+	// get the file's UTI
+	NSString * uti = [[fileURL resourceValuesForKeys: [NSArray arrayWithObject: NSURLTypeIdentifierKey] error: NULL] objectForKey: NSURLTypeIdentifierKey];
+	
+	// remove any old usro resource
+	// if an app wasn't specified then we only remove the resource
+	RemoveUsroResource( fileURL );
+	if ( appURL == nil )
+	{
+		[fileURL setResourceValue: [NSNull null] forKey: NSURLCustomIconKey error: NULL];
+		return ( EX_OK );
+	}
+	
+	InstallUsroResource( fileURL, appURL );
+	NSImage * customIcon = GetAppFileIcon( appURL, fileURL, uti );
+	if ( customIcon != nil )
+	{
+		if ( [fileURL setResourceValue: customIcon forKey: NSURLCustomIconKey error: NULL] == NO )
+		{
+			fprintf( stderr, "Failed to set custom icon for file.\n" );
+			fflush( stderr );
+			exit( EX_SOFTWARE );
+		}
+	}
+	
+    return ( EX_OK );
+}
